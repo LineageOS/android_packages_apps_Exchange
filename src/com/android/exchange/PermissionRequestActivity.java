72d808ba13bf33b3b1eaa272816054d0d5bc8103
monojp <martin@herndl.org>
2018-04-06 23:49:31 +0200

    Exchange: request permissions
    Change-Id: I3fe87cc75d5152aa847b539cec7d5f60f7c5ed44

---
+++ src/com/android/exchange/PermissionRequestActivity.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.exchange;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+public class PermissionRequestActivity extends Activity
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
+    private static final String TAG = Eas.LOG_TAG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String[] permissions = intent.getStringArrayExtra("permissions");
+        if (permissions == null) {
+            Log.e(TAG, "No permission StringArray found in intent for PermissionRequestActivity");
+        } else {
+            int requestCode = intent.getIntExtra("requestCode", 0);
+            if (!permissionsGranted(permissions)) {
+                requestPermissions(permissions, requestCode);
+            }
+        }
+
+        finish();
+    }
+
+    private boolean permissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        for (int i = 0; i < permissions.length; i++) {
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "Permission " + permissions[i] + " was not granted, request code was " + requestCode);
+            }
+        }
+    }
+}
